adbd604b8c22e51fc1150b3a87eaccf603b28777
thomaspauls <48553414+thomaspauls@users.noreply.github.com>
2021-06-07 16:17:10 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def hello_world():
-    return "<h1>Hello world from K8s Kubernetes!</h1>"
+    return "<h1>Hello world from K8s Kubernetes!</h1><h2>Testing the cicd pipeline</h2>"
 
 
 if __name__ == '__main__':
